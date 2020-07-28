32632035561a18a029ea558e6d14e861dcf98f41
vitalii.adler <adler.vitalii@gmail.com>
2020-07-28 15:14:51 +0300

    added tasks

--- interview_tasks.js
+++ interview_tasks.js
@@ -1,3 +1,20 @@
 // https://github.com/ganqqwerty/123-Essential-JavaScript-Interview-Questions/blob/master/README.md
 
 // https://www.strictmode.io/articles/javascript-interview-exercises-with-solutions-2019/
+/**
+ * 1 task
+ */
+// / alphabetical comparison (a < zzzz, aaaa < z)
+// case-insensitive comparison (air == AIR)
+// equality operators (==, >=, <=, !=) forbidden for whole strings
+
+function string_cmp(a, b) {
+return a.localeCompare(b)
+
+//	return 0 if a == b
+//	return any negative number if a < b
+//	return any positive number if a > b
+
+}
+
+console.log(string_cmp('b','a'))
